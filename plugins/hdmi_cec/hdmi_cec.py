0cbded2c911cfd991a27e0b41995131020f7388f
Václav Chlumský <chlumskyvaclav@gmail.com>
2017-05-06 08:34:31 +0200

    cec sys path update

--- plugins/hdmi_cec/hdmi_cec.py
+++ plugins/hdmi_cec/hdmi_cec.py
@@ -16,7 +16,7 @@ import cfg
 import sys
 
 try:
-    sys.path.append('/usr/osmc/lib/python2.7/site-packages/')
+    sys.path.append('/usr/osmc/lib/python2.7/dist-packages/')
     import cec
 except ImportError:
     raise Exception('Importing cec failed!')
